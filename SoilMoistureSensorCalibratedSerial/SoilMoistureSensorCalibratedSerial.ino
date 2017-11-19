0f7170b537dd49a3e45b48410098ec0da4eae454
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-19 07:54:15 +0000

    Reorganised code trying to divide into smaller files

--- SoilMoistureSensorCalibratedSerial/SoilMoistureSensorCalibratedSerial.ino
+++ SoilMoistureSensorCalibratedSerial/SoilMoistureSensorCalibratedSerial.ino
@@ -1,73 +1,39 @@
 #include <Arduino.h>
 #include <EEPROM.h>
 
-long second = 1000;
-long minute = second * 60;
-long hour = minute * 60;
+extern bool isDebugMode = false;
 
-long lastReadingTime = 0;
-
-//long readingInterval = 1 * hour;
-//long readingInterval = 1 * minute;
-//long readingInterval = 5 * minute;
-//long readingInterval = 1 * second;
-long readingInterval = 3 * second;
-
-#define moisturePin 0
-#define sensorPowerPin 12
-
-int moistureLevel = 0;
-int moistureLevelRaw = 0;
-
-bool reverseSoilMoistureSensor = false;
-int dryReading = (reverseSoilMoistureSensor ? 0 : 1024);
-int wetReading = (reverseSoilMoistureSensor ? 1024 : 0);
-
-int flagAddress = 0;
-int dryReadingAddress = 2;
-int wetReadingAddress = 3;
-
-bool sensorIsOn = true;
-long lastSensorOnTime = 0;
-int delayAfterTurningSensorOn = 3 * second;
 
 long lastSerialOutputTime = 0;
-long serialOutputInterval = readingInterval;
+long serialOutputInterval = 3 * 1000;//soilMoistureSensorReadingInterval;
 
-bool isDebug = false;
-
-#define SERIAL_MODE_CALIBRATED 1
+/*#define SERIAL_MODE_CALIBRATED 1
 #define SERIAL_MODE_RAW 2
 #define SERIAL_MODE_CSV 3
 #define SERIAL_MODE_QUERYSTRING 4
 
-int serialMode = SERIAL_MODE_CSV;
+int serialMode = SERIAL_MODE_CSV;*/
+
+
+#include "SoilMoistureSensor.h"
 
 void setup()
 {
+  
   Serial.begin(9600);
   //Serial.begin(115200);
 
-  if (isDebug)
-    Serial.println("Starting soil moisture sensor");
+  //if (isDebug)
+  //  Serial.println("Starting soil moisture sensor");
 
-  pinMode(sensorPowerPin, OUTPUT);
-
-  // If the interval is less than 2 seconds turn the sensor on and leave it on (otherwise it will be turned on just before it's needed)
-  if (readingInterval <= delayAfterTurningSensorOn)
-  {
-    sensorOn();
-
-    delay(delayAfterTurningSensorOn);
-  }
-
+  setupSoilMoistureSensor();
 }
 
 void loop()
 {
   checkCommand();
 
-  takeReading();
+  takeSoilMoistureSensorReading();
 
   serialPrintData();
 
@@ -86,109 +52,28 @@ void checkCommand()
     switch (command)
     {
       case 'D':
-        lastReadingTime = 0;
-        takeReading();
-        setDryReading(moistureLevelRaw);
+       // lastSoilMoistureSensorReadingTime = 0;
+     //   takeSoilMoistureSensorReading();
+    //    setDrySoilMoistureCalibrationValue(soilMoistureLevelRaw);
         break;
-      case 'W':
-        lastReadingTime = 0;
-        takeReading();
-        setWetReading(moistureLevelRaw);
+    /*  case 'W':
+        lastSoilMoistureSensorReadingTime = 0;
+        takeSoilMoistureSensorReading();
+        setWetSoilMoistureCalibrationValue(soilMoistureLevelRaw);
         break;
       case 'Z':
         Serial.println("Toggling IsDebug");
-        isDebug = !isDebug;
-        break;
+        isDebugMode = !isDebugMode;
+        break;*/
     }
   }
 }
 
-/* Readings */
-void takeReading()
-{
-  bool sensorReadingIsDue = lastReadingTime + readingInterval < millis() || lastReadingTime == 0;
-
-  if (sensorReadingIsDue)
-  {
-      if (isDebug)
-        Serial.println("Sensor reading is due");
-
-	bool sensorGetsTurnedOff = readingInterval > delayAfterTurningSensorOn;
-
-	bool sensorIsOffAndNeedsToBeTurnedOn = !sensorIsOn && sensorGetsTurnedOff;
-
-	bool postSensorOnDelayHasPast = lastSensorOnTime + delayAfterTurningSensorOn < millis();
-
-	bool sensorIsOnAndReady = sensorIsOn && (postSensorOnDelayHasPast || !sensorGetsTurnedOff);
-
-    if (isDebug)
-    {
-        Serial.print("Sensor gets turned off: ");
-        Serial.println(sensorGetsTurnedOff);
-        Serial.print("Sensor is off and needs to be turned on: ");
-        Serial.println(sensorIsOffAndNeedsToBeTurnedOn);
-        Serial.print("Post sensor on delay has past: ");
-        Serial.println(postSensorOnDelayHasPast);
-        Serial.print("Sensor is on and ready: ");
-        Serial.println(sensorIsOnAndReady);
-    }
-
-    if (sensorIsOffAndNeedsToBeTurnedOn)
-    {
-      sensorOn();
-    }
-    else if (sensorIsOnAndReady)
-    {
-      if (isDebug)
-        Serial.println("Preparing to take reading");
-
-      lastReadingTime = millis();
-
-      moistureLevelRaw = getAverageReading();
-
-      moistureLevel = calculateMoistureLevel(moistureLevelRaw);
-
-      if (moistureLevel < 0)
-        moistureLevel = 0;
-
-      if (moistureLevel > 100)
-        moistureLevel = 100;
-
-      // If the interval is less than 2 seconds then don't turn the sensor off
-      if (readingInterval > delayAfterTurningSensorOn)
-      {
-        sensorOff();
-      }
-    }
-  }
-}
-
-double getAverageReading()
-{
-  int readingSum  = 0;
-  int totalReadings = 10;
-
-  for (int i = 0; i < totalReadings; i++)
-  {
-    int reading = analogRead(moisturePin);
-
-    readingSum += reading;
-  }
-
-  double averageReading = readingSum / totalReadings;
-
-  return averageReading;
-}
-
-int calculateMoistureLevel(int reading)
-{
-  return map(reading, dryReading, wetReading, 0, 100);
-}
 
 /* Serial Output */
 void serialPrintData()
 {
-  if (lastSerialOutputTime + serialOutputInterval < millis()
+  /*if (lastSerialOutputTime + serialOutputInterval < millis()
       || lastSerialOutputTime == 0)
   {
 	long numberOfSecondsOnline = millis()/1000;
@@ -200,16 +85,16 @@ void serialPrintData()
       Serial.print(numberOfSecondsOnline);
       Serial.print(";");
       Serial.print("R:");
-      Serial.print(moistureLevelRaw);
+      Serial.print(soilMoistureLevelRaw);
       Serial.print(";");
       Serial.print("C:");
-      Serial.print(moistureLevel);
+      Serial.print(soilMoistureLevelCalibrated);
       Serial.print(";");
       Serial.print("D:");
-      Serial.print(dryReading);
+      Serial.print(drySoilMoistureCalibrationValue);
       Serial.print(";");
       Serial.print("W:");
-      Serial.print(wetReading);
+      Serial.print(wetSoilMoistureCalibrationValue);
       Serial.print(";");
       Serial.println();
     }
@@ -219,130 +104,27 @@ void serialPrintData()
       Serial.print(numberOfSecondsOnline);
       Serial.print("&");
       Serial.print("raw=");
-      Serial.print(moistureLevelRaw);
+      Serial.print(soilMoistureLevelRaw);
       Serial.print("&");
       Serial.print("calibrated=");
-      Serial.print(moistureLevel);
+      Serial.print(soilMoistureLevelCalibrated);
       Serial.print("&");
       Serial.print("dry=");
-      Serial.print(dryReading);
+      Serial.print(drySoilMoistureCalibrationValue);
       Serial.print("&");
       Serial.print("wet=");
-      Serial.print(wetReading);
+      Serial.print(wetSoilMoistureCalibrationValue);
       Serial.println();
     }
 	else if (serialMode == SERIAL_MODE_CALIBRATED)
 	{
-      Serial.println(moistureLevel);
+      Serial.println(soilMoistureLevelCalibrated);
 	}
 	else if (serialMode == SERIAL_MODE_RAW)
 	{
-      Serial.println(moistureLevelRaw);
+      Serial.println(soilMoistureLevelRaw);
 	}
 
     lastSerialOutputTime = millis();
-  }
+  }*/
 }
-
-/* Sensor */
-void sensorOn()
-{
-  if (isDebug)
-    Serial.println("Turning sensor on");
-
-  digitalWrite(sensorPowerPin, HIGH);
-
-  lastSensorOnTime = millis();
-
-  delay(delayAfterTurningSensorOn);
-
-  sensorIsOn = true;
-}
-
-void sensorOff()
-{
-  if (isDebug)
-    Serial.println("Turning sensor off");
-
-  digitalWrite(sensorPowerPin, LOW);
-
-  sensorIsOn = false;
-}
-
-/* Calibration */
-void setDryReading(int reading)
-{
-  dryReading = reading;
-
-  if (isDebug)
-  {
-    Serial.print("Setting dry reading to EEPROM: ");
-    Serial.println(reading);
-  }
-
-  int compactValue = reading / 4;
-
-  EEPROM.write(dryReadingAddress, compactValue); // Must divide by 4 to make it fit in eeprom
-
-  setEEPROMFlag();
-}
-
-void setWetReading(int reading)
-{
-  wetReading = reading;
-
-  if (isDebug)
-  {
-    Serial.print("Setting wet reading to EEPROM: ");
-    Serial.println(reading);
-  }
-
-  int compactValue = reading / 4;
-
-  EEPROM.write(wetReadingAddress, compactValue); // Must divide by 4 to make it fit in eeprom
-  
-  setEEPROMFlag();
-}
-
-int getDryReading()
-{
-  int value = EEPROM.read(dryReadingAddress);
-
-  if (value == 0
-      || value == 255)
-    return dryReading;
-  else
-  {
-    int dryReading = value * 4; // Must multiply by 4 to get the original value
-  
-    if (isDebug)
-    {
-      Serial.print("Dry reading found in EEPROM: ");
-      Serial.println(dryReading);
-    }
-
-    return dryReading;
-  }
-}
-
-int getWetReading()
-{
-  int value = EEPROM.read(wetReadingAddress);
-
-  int wetReading = value * 4; // Must multiply by 4 to get the original value
-
-  if (isDebug)
-  {
-    Serial.print("Wet reading found in EEPROM: ");
-    Serial.println(wetReading);
-  }
-
-  return wetReading;
-}
-
-void setEEPROMFlag()
-{
-  if (EEPROM.read(flagAddress) != 99)
-    EEPROM.write(flagAddress, 99);
-}
-
