abb7d0a59daecef013e27fd99227943b09b52a71
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-23 01:49:16 +0000

    Updated prepare script

--- prepare.sh
+++ prepare.sh
@@ -2,8 +2,8 @@
 
 # Note: You may need to run this script with sudo
 
-apt update &&
-apt -y install curl &&
+apt-get update &&
+apt-get -y install curl &&
 python -c "$(curl -fsSL https://raw.githubusercontent.com/platformio/platformio/develop/scripts/get-platformio.py)"
 
 # Or
