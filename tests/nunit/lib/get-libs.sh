6c49acbbcda86c6005853a74ba49219f0df9debf
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-27 01:32:26 +0000

    Updated get-libs to clone and build duinocom

--- tests/nunit/lib/get-libs.sh
+++ tests/nunit/lib/get-libs.sh
@@ -12,3 +12,8 @@ fi
 
 mono nuget.exe install nunit -version 2.6.4
 mono nuget.exe install nunit.runners -version 2.6.4
+
+git clone http://gitub.com/CompulsiveCoder/duinocom.git
+cd duinocom
+sh init.sh &&
+sh build.sh
