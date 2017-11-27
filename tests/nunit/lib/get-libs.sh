8999ba1926a68cc4d81cccf58e68d2b9cbf1cdfb
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-27 05:00:13 +0000

    Added prepare script call to get-libs

--- tests/nunit/lib/get-libs.sh
+++ tests/nunit/lib/get-libs.sh
@@ -15,5 +15,6 @@ mono nuget.exe install nunit.runners -version 2.6.4
 
 git clone http://github.com/CompulsiveCoder/ArduinoSerialControllerClient.git
 cd ArduinoSerialControllerClient
+sh prepare.sh &&
 sh init.sh &&
 sh build.sh
