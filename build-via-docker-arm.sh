13f59ae1aaae911242b712ce4add833863b050fc
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-28 03:55:18 +0000

    Updated build-via-docker-arm script to call build-all

--- build-via-docker-arm.sh
+++ build-via-docker-arm.sh
@@ -1,2 +1,2 @@
 
-docker run -i -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/SoilMoistureSensorCalibratedSerial compulsivecoder/ubuntu-arm-iot-mono /bin/bash -c "rsync -avzh /SoilMoistureSensorCalibratedSerial/ /project && cd /project && sh prepare.sh && sh init.sh && sh build.sh"
+docker run -i -v /var/run/docker.sock:/var/run/docker.sock -v $PWD:/SoilMoistureSensorCalibratedSerial compulsivecoder/ubuntu-arm-iot-mono /bin/bash -c "rsync -avzh /SoilMoistureSensorCalibratedSerial/ /project && cd /project && sh prepare.sh && sh init.sh && sh build-all.sh"
