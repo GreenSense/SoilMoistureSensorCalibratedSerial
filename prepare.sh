55008f031fb6182bfb1690ffee0878878b3187d1
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-28 03:51:15 +0000

    Fixed prepare

--- prepare.sh
+++ prepare.sh
@@ -26,7 +26,7 @@ if ! type "mono" > /dev/null; then
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
   echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 
-  sudo apt-get install -y mono-devel mono-complete ca-certificates-mono
+  sudo apt-get update && apt-get install -y mono-devel mono-complete ca-certificates-mono
 fi
 
 cd tests/nunit && \
