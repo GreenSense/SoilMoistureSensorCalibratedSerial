a8961d929d1515ff0dc69363bb3a3b1a61d71b2c
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-28 03:58:53 +0000

    Updated prepare script

--- prepare.sh
+++ prepare.sh
@@ -22,7 +22,7 @@ if ! type "pio" > /dev/null; then
 fi
 
 # mono
-if ! type "mono" > /dev/null; then
+if ! type "msbuild" > /dev/null; then
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
   echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 
