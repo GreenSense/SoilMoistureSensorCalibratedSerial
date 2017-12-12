1c0138dd8403187f36a21d66cc632feb2ee7a2a3
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-12 05:13:19 +0000

    Added mock test script

---
+++ test-via-docker-from-github-mock-success.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Finished test cycle"
+echo "=============================="
+echo ""
+echo ""
+echo "Tests run: 1, Errors: 0, Failures: 0, Inconclusive: 0, Time: 208.3802504 seconds"
+echo "Not run: 0, Invalid: 0, Ignored: 0, Skipped: 0"
+echo ""
+echo "WARNING: The runtime version supported by this application is unavailable."
+echo "Using default runtime: v4.0.30319"
