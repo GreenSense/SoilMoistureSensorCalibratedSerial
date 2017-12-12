e28532b97c0613793557b3ffb2811fdeb83d1479
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-12 07:39:32 +0000

    Added more console output to upload-simulator script

--- upload-simulator.sh
+++ upload-simulator.sh
@@ -1,5 +1,9 @@
 # May need be run as sudo
 
+echo ""
+echo "Uploading simulator sketch"
+echo ""
+
 # Specify a temporary directory name
 SIMULATOR_TMP_DIR="_simulatortmp"
 
@@ -9,6 +13,10 @@ rm -rf $SIMULATOR_TMP_DIR
 # Make a new directory
 mkdir -p $SIMULATOR_TMP_DIR
 cd $SIMULATOR_TMP_DIR
+
+echo "Preparing to clone ArduinoSerialController"
+echo "Dir:"
+echo "  $PWD"
 
 # Clone the latest version
 git clone https://github.com/CompulsiveCoder/ArduinoSerialController.git
