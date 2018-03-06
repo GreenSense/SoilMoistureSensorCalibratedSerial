e6f81f1a21bd5229780e7c41d49fa5ed752b4af3
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-06 23:35:04 +0000

    Added/updated integration tests

---
+++ tests/nunit/src/SoilMoistureSensorCalibratedSerial.Tests.Integration/CalibrateCommandTestFixture.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using NUnit.Framework;
+using duinocom;
+using System.Threading;
+using ArduinoSerialControllerClient;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SoilMoistureSensorCalibratedSerial.Tests.Integration
+{
+	[TestFixture(Category="Integration")]
+	public class CalibrateCommandTestFixture : BaseTestFixture
+	{
+		[Test]
+		public void Test_CalibrateDryCommand()
+		{
+			var percentage = 20;
+
+			var raw = 219;//(percentage * 1024 / 100)-1;
+
+			TestCalibrationCommand ("dry", "D", percentage, raw);
+		}
+
+		[Test]
+		public void Test_CalibrateWetCommand()
+		{
+			var percentage = 80;
+
+			var raw = 880;//percentage * 1023 / 100;
+
+			TestCalibrationCommand ("wet", "W", percentage, raw);
+		}
+
+		public void TestCalibrationCommand(string label, string command, int percentageIn, int expectedRaw)
+		{
+
+			Console.WriteLine ("");
+			Console.WriteLine ("==============================");
+			Console.WriteLine ("Starting calibrate " + label + " command test");
+			Console.WriteLine ("");
+			Console.WriteLine ("Percentage in: " + percentageIn);
+			Console.WriteLine ("Expected raw: " + expectedRaw);
+
+			SerialClient soilMoistureMonitor = null;
+			ArduinoSerialDevice soilMoistureSimulator = null;
+
+			try {
+				soilMoistureMonitor = new SerialClient ("/dev/ttyUSB0", 9600);
+				soilMoistureSimulator = new ArduinoSerialDevice ("/dev/ttyUSB1", 9600);
+
+				Console.WriteLine("");
+				Console.WriteLine("Connecting to serial devices...");
+				Console.WriteLine("");
+
+				soilMoistureMonitor.Open ();
+				soilMoistureSimulator.Connect ();
+
+				Thread.Sleep (4000);
+
+
+				Console.WriteLine("");
+				Console.WriteLine("Sending 'X' command to device to reset to defaults...");
+				Console.WriteLine("");
+
+				// Reset defaults
+				soilMoistureMonitor.WriteLine ("X");
+
+				Thread.Sleep(5000);
+
+				Console.WriteLine("");
+				Console.WriteLine("Reading the output from the monitor...");
+				Console.WriteLine("");
+
+				// Read the output
+				var outputLine = soilMoistureMonitor.Read ();
+
+				Console.WriteLine (outputLine);
+				Console.WriteLine ("");
+
+				Thread.Sleep(2000);
+
+				Console.WriteLine("");
+				Console.WriteLine("Sending analog percentage to simulator: " + percentageIn);
+				Console.WriteLine("");
+
+				// Set the simulated soil moisture
+				soilMoistureSimulator.AnalogWritePercentage (9, percentageIn);
+
+				Thread.Sleep(12000);
+
+				Console.WriteLine("");
+				Console.WriteLine("Reading output from the monitor device...");
+				Console.WriteLine("");
+				// Read the output
+				outputLine = soilMoistureMonitor.Read ();
+
+				Console.WriteLine (outputLine);
+				Console.WriteLine ("");
+
+				// Extract the data line
+				var dataLine = "";
+
+				var dataLines = outputLine.Split('\n');
+
+				for (int i = dataLines.Length-1; i>=0; i--)
+				{
+					if (dataLines[i].StartsWith("D;"))
+					{
+						dataLine = dataLines[i];
+						break;
+					}
+				}
+
+				Console.WriteLine ("Data line:");
+				Console.WriteLine (dataLine);
+				Console.WriteLine ("");
+
+				// Parse the values in the data line
+				var values = ParseOutputLine(dataLine);
+
+				// Get the raw soil moisture value
+				var rawValue = values["R"];
+
+
+				Console.WriteLine("");
+				Console.WriteLine("Checking the values from the monitor device...");
+				Console.WriteLine("");
+
+				// Ensure the raw value is in the valid range
+				Assert.IsTrue(rawValue >= expectedRaw-3 && rawValue <= expectedRaw+3, "Raw value is outside the valid range: " + rawValue);
+
+
+				Console.WriteLine("");
+				Console.WriteLine("Sending '" + command + "' command to monitor device...");
+				Console.WriteLine("");
+
+				// Send the command
+				soilMoistureMonitor.WriteLine (command);
+
+				Thread.Sleep(2000);
+
+				Console.WriteLine("");
+				Console.WriteLine("Reading the output from the monitor device...");
+				Console.WriteLine("");
+
+				// Read the output
+				outputLine = soilMoistureMonitor.Read ();
+
+				Console.WriteLine (outputLine);
+				Console.WriteLine ("");
+
+				Console.WriteLine("");
+				Console.WriteLine("Checking the output...");
+				Console.WriteLine("");
+
+				// Check the output
+				var expected = "Setting " + label + " soil moisture sensor calibration value:";
+				Assert.IsTrue(outputLine.Contains(expected), "Didn't find expected output");
+
+				var lastLine = "";
+
+				var lines = outputLine.Split('\n');
+
+				// Extract the line containing the calibration value
+				for (int i = lines.Length-1; i>=0; i--)
+				{
+					var line = lines[i];
+					if (line.StartsWith(expected))
+					{
+						lastLine = line;
+						break;
+					}
+				}
+
+				Console.WriteLine("Last line");
+				Console.WriteLine(lastLine);
+				Console.WriteLine("");
+
+				// Extraction the calibration value
+				var cvString = lastLine.Substring(lastLine.IndexOf(":")+2, 3);
+				var calibrationValue = Convert.ToInt32(cvString);
+
+				Console.WriteLine("Calibration value: " + calibrationValue);
+				Console.WriteLine("");
+
+				// Ensure the calibration value is in the valid range
+				Assert.IsTrue(calibrationValue >= expectedRaw-3 && calibrationValue <= expectedRaw+3, "Calibration value is outside the valid range: " + calibrationValue);
+
+			} catch (IOException ex) {
+				Console.WriteLine (ex.ToString ());
+			} finally {
+				if (soilMoistureMonitor != null)
+					soilMoistureMonitor.Close ();
+
+				if (soilMoistureSimulator != null)
+					soilMoistureSimulator.Disconnect ();
+			}
+		}
+
+		/*[Test]
+		public void Test_CalibrateWetCommand()
+		{
+			Console.WriteLine ("");
+			Console.WriteLine ("==============================");
+			Console.WriteLine ("Starting calibrate wet command test");
+			Console.WriteLine ("");
+
+			SerialClient soilMoistureMonitor = null;
+			ArduinoSerialDevice soilMoistureSimulator = null;
+
+			try {
+				soilMoistureMonitor = new SerialClient ("/dev/ttyUSB0", 9600);
+				soilMoistureSimulator = new ArduinoSerialDevice ("/dev/ttyUSB1", 9600);
+
+				soilMoistureMonitor.Open ();
+				soilMoistureSimulator.Connect ();
+
+				Thread.Sleep (4000);
+
+				// Reset defaults
+				soilMoistureMonitor.WriteLine ("X");
+
+				// Set the simulated soil moisture to 20%
+				soilMoistureSimulator.AnalogWritePercentage (9, 80);
+
+				Thread.Sleep(1000);
+
+				// Read the output
+				var outputLine = soilMoistureMonitor.Read ();
+
+				Console.WriteLine (outputLine);
+				Console.WriteLine ("");
+
+				// Extract the data line
+				var dataLine = (from line in outputLine.Split('\n')
+					where line.StartsWith("D;")
+					select line).FirstOrDefault();
+
+				// Parse the values in the data line
+				var values = ParseOutputLine(dataLine);
+
+				// Get the raw soil moisture value
+				var rawValue = values["R"];
+
+				// Ensure the raw value is in the valid range
+				Assert.IsTrue(rawValue >= 218 && rawValue <= 222, "Raw value is outside the valid range.");
+
+				// Send the "D" (dry) command
+				soilMoistureMonitor.WriteLine ("W");
+
+				Thread.Sleep(1000);
+
+				// Read the output
+				outputLine = soilMoistureMonitor.Read ();
+
+				Console.WriteLine (outputLine);
+				Console.WriteLine ("");
+
+				// Check the output
+				var expected = "Setting dry soil moisture sensor calibration value:";
+				Assert.IsTrue(outputLine.Contains(expected), "Didn't find expected output");
+
+				var lastLine = "";
+
+				var lines = outputLine.Split('\n');
+
+				// Extract the line containing the calibration value
+				for (int i = lines.Length-1; i>=0; i--)
+				{
+					var line = lines[i];
+					if (line.StartsWith(expected))
+					{
+						lastLine = line;
+						break;
+					}
+				}
+
+				Console.WriteLine("Last line");
+				Console.WriteLine(lastLine);
+				Console.WriteLine("");
+
+				// Extraction the calibration value
+				var cvString = lastLine.Substring(lastLine.IndexOf(":")+2, 3);
+				var calibrationValue = Convert.ToInt32(cvString);
+
+				Console.WriteLine("Calibration value: " + calibrationValue);
+				Console.WriteLine("");
+
+				// Ensure the calibration value is in the valid range
+				Assert.IsTrue(calibrationValue >= 218 && calibrationValue <= 222, "Calibration value is outside the valid range.");
+
+			} catch (IOException ex) {
+				Console.WriteLine (ex.ToString ());
+			} finally {
+				if (soilMoistureMonitor != null)
+					soilMoistureMonitor.Close ();
+
+				if (soilMoistureSimulator != null)
+					soilMoistureSimulator.Disconnect ();
+			}
+		}*/
+
+		public Dictionary<string, int> ParseOutputLine(string outputLine)
+		{
+			var dictionary = new Dictionary<string, int> ();
+
+			if (IsValidOutputLine (outputLine)) {
+				foreach (var pair in outputLine.Split(';')) {
+					var parts = pair.Split (':');
+
+					if (parts.Length == 2) {
+						var key = parts [0];
+						var value = 0;
+						try {
+							value = Convert.ToInt32 (parts [1]);
+
+							dictionary [key] = value;
+						} catch {
+							Console.WriteLine ("Warning: Invalid key/value pair '" + pair + "'");
+						}
+					}
+				}
+			}
+
+			return dictionary;
+		}
+
+		public bool IsValidOutputLine(string outputLine)
+		{
+			var dataPrefix = "D;";
+
+			return outputLine.StartsWith(dataPrefix);
+		}
+	}
+}
--- tests/nunit/src/SoilMoistureSensorCalibratedSerial.Tests.Integration/FullScaleTestFixture.cs
+++ tests/nunit/src/SoilMoistureSensorCalibratedSerial.Tests.Integration/FullScaleTestFixture.cs
@@ -14,144 +14,131 @@ namespace SoilMoistureSensorCalibratedSerial.Tests.Integration
 		[Test]
 		public void Test_Complete()
 		{
-      Console.WriteLine("");
-      Console.WriteLine("==============================");
-      Console.WriteLine("Starting full scale test");
-      Console.WriteLine("");
+			Console.WriteLine ("");
+			Console.WriteLine ("==============================");
+			Console.WriteLine ("Starting full scale test");
+			Console.WriteLine ("");
 
-      SerialClient soilMoistureMonitor = null;
-      ArduinoSerialDevice soilMoistureSimulator = null;
+			SerialClient soilMoistureMonitor = null;
+			ArduinoSerialDevice soilMoistureSimulator = null;
 
-      try
-      {
-        soilMoistureMonitor = new SerialClient("/dev/ttyUSB0", 9600);
-        soilMoistureSimulator = new ArduinoSerialDevice("/dev/ttyUSB1", 9600);
+			try {
+				soilMoistureMonitor = new SerialClient ("/dev/ttyUSB0", 9600);
+				soilMoistureSimulator = new ArduinoSerialDevice ("/dev/ttyUSB1", 9600);
       
-        soilMoistureMonitor.Open();
-        soilMoistureSimulator.Connect();
+				soilMoistureMonitor.Open ();
+				soilMoistureSimulator.Connect ();
 
-        Thread.Sleep(5000);
+				Thread.Sleep (5000);
 
-      	// Reset defaults
-      	soilMoistureMonitor.WriteLine("X");
+				// Reset defaults
+				soilMoistureMonitor.WriteLine ("X");
 
-        int step = 25;
+				int step = 25;
 
-        for (int i = 100; i >= 0; i-=step)
-        {
-            RunCalibrationTest(i, CalibrationIsReversedByDefault, soilMoistureMonitor, soilMoistureSimulator);
+				for (int i = 100; i >= 0; i -= step) {
+					RunCalibrationTest (i, CalibrationIsReversedByDefault, soilMoistureMonitor, soilMoistureSimulator);
         
-            Thread.Sleep(1000);
-        }
+					Thread.Sleep (1000);
+				}
         
-        for (int i = 0; i < 100; i+=step)
-        {
-            RunCalibrationTest(i, CalibrationIsReversedByDefault, soilMoistureMonitor, soilMoistureSimulator);
+				for (int i = 0; i < 100; i += step) {
+					RunCalibrationTest (i, CalibrationIsReversedByDefault, soilMoistureMonitor, soilMoistureSimulator);
 
-            Thread.Sleep(1000);
-        }
+					Thread.Sleep (1000);
+				}
 
-      }
-      catch(IOException ex)
-      {
-        Console.WriteLine(ex.ToString());
-      }
-      finally
-      {
-        if (soilMoistureMonitor != null)
-          soilMoistureMonitor.Close();
+			} catch (IOException ex) {
+				Console.WriteLine (ex.ToString ());
+			} finally {
+				if (soilMoistureMonitor != null)
+					soilMoistureMonitor.Close ();
           
-        if (soilMoistureSimulator != null)
-          soilMoistureSimulator.Disconnect();
-      }
+				if (soilMoistureSimulator != null)
+					soilMoistureSimulator.Disconnect ();
+			}
 		}
 		
 		public void RunCalibrationTest(int soilMoisturePercentage, bool calibrationIsReversed, SerialClient soilMoistureMonitor, ArduinoSerialDevice soilMoistureSimulator)
 		{
 		
-      Console.WriteLine("");
-      Console.WriteLine("==============================");
-      Console.WriteLine("Starting calibration test");
-      Console.WriteLine("");
+			Console.WriteLine ("");
+			Console.WriteLine ("==============================");
+			Console.WriteLine ("Starting calibration test");
+			Console.WriteLine ("");
       
       
-      int percentageValue = soilMoisturePercentage;
+			int percentageValue = soilMoisturePercentage;
       
         
       
-      Console.WriteLine("");
-      Console.WriteLine("Sending percentage to simulator: " + percentageValue);
+			Console.WriteLine ("");
+			Console.WriteLine ("Sending percentage to simulator: " + percentageValue);
       
-      soilMoistureSimulator.AnalogWritePercentage(9, percentageValue);
+			soilMoistureSimulator.AnalogWritePercentage (9, percentageValue);
       
-      Thread.Sleep(10000);
+			Thread.Sleep (10000);
       
-      Console.WriteLine("");
-      Console.WriteLine("Reading data from soil moisture monitor");
+			Console.WriteLine ("");
+			Console.WriteLine ("Reading data from soil moisture monitor");
       
-      var outputLine = soilMoistureMonitor.Read();
+			var outputLine = soilMoistureMonitor.Read ();
       
-      Console.WriteLine(outputLine);
-      Console.WriteLine("");
+			Console.WriteLine (outputLine);
+			Console.WriteLine ("");
       
-      var data = ParseOutputLine(outputLine);
+			var data = ParseOutputLine (outputLine);
       
       
-      Console.WriteLine("");
-      Console.WriteLine("Checking calibrated value");
-      var expectedCalibratedValue = percentageValue;
+			Console.WriteLine ("");
+			Console.WriteLine ("Checking calibrated value");
+			var expectedCalibratedValue = percentageValue;
       
-      if (calibrationIsReversed)
-        expectedCalibratedValue = ArduinoConvert.ReversePercentage(percentageValue);
+			if (calibrationIsReversed)
+				expectedCalibratedValue = ArduinoConvert.ReversePercentage (percentageValue);
       
-      var calibratedValueIsWithinRange = IsWithinRange(expectedCalibratedValue, data["C"], 8);
+			var calibratedValueIsWithinRange = IsWithinRange (expectedCalibratedValue, data ["C"], 8);
       
-      Assert.IsTrue(calibratedValueIsWithinRange, "Invalid value for 'C' (calibrated value).");
+			Assert.IsTrue (calibratedValueIsWithinRange, "Invalid value for 'C' (calibrated value).");
       
-      Console.WriteLine("");
-      Console.WriteLine("Checking raw value");
+			Console.WriteLine ("");
+			Console.WriteLine ("Checking raw value");
       
-      var expectedRawValue = ArduinoConvert.PercentageToAnalog(percentageValue);
+			var expectedRawValue = ArduinoConvert.PercentageToAnalog (percentageValue);
       
-      var rawValueIsWithinRange = IsWithinRange(expectedRawValue, data["R"], 80);
+			var rawValueIsWithinRange = IsWithinRange (expectedRawValue, data ["R"], 80);
       
-      Assert.IsTrue(rawValueIsWithinRange, "Invalid value for 'R' (raw value).");
+			Assert.IsTrue (rawValueIsWithinRange, "Invalid value for 'R' (raw value).");
       
-      Console.WriteLine("");
-      Console.WriteLine("Finished calibration test");
-      Console.WriteLine("==============================");
-      Console.WriteLine("");
+			Console.WriteLine ("");
+			Console.WriteLine ("Finished calibration test");
+			Console.WriteLine ("==============================");
+			Console.WriteLine ("");
 		}
 		
 		public Dictionary<string, int> ParseOutputLine(string outputLine)
 		{
-		  var dictionary = new Dictionary<string, int>();
+			var dictionary = new Dictionary<string, int> ();
 		  
-		  if (IsValidOutputLine(outputLine))
-		  {
-  		  foreach (var pair in outputLine.Split(';'))
-  		  {
-  		    var parts = pair.Split(':');
+			if (IsValidOutputLine (outputLine)) {
+				foreach (var pair in outputLine.Split(';')) {
+					var parts = pair.Split (':');
   		    
-  		    if (parts.Length == 2)
-  		    {
-  		      var key = parts[0];
-  		      var value = 0;
-  		      try
-  		      {
-  		        value = Convert.ToInt32(parts[1]);
+					if (parts.Length == 2) {
+						var key = parts [0];
+						var value = 0;
+						try {
+							value = Convert.ToInt32 (parts [1]);
   		      
-  		        dictionary[key] = value;
-  		      }
-  		      catch
-  		      {
-  		        Console.WriteLine("Warning: Invalid key/value pair '" + pair + "'");
-  		      }
-  		    }
-  		  }
-		  }
+							dictionary [key] = value;
+						} catch {
+							Console.WriteLine ("Warning: Invalid key/value pair '" + pair + "'");
+						}
+					}
+				}
+			}
 		  
-		  return dictionary;
+			return dictionary;
 		}
 		
 		public bool IsValidOutputLine(string outputLine)
