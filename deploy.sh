2f9c264af09c9d168833e2cbda07682b336b57a2
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-15 23:31:37 +0000

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,7 @@ if [ "$BRANCH" = "master" ]
 then
   echo "Deploying to host: $GARDEN_HOSTNAME"
   
-  # Messy hack to get deployment working. This should be installed by default into the jenkins container but that doesnt seem to be working
-  sudo apt-get update && sudo apt-get -y install sshpass
-
-  sshpass -p $GARDEN_PASSWORD ssh $GARDEN_USER:$GARDEN_PASSWORD@$GARDEN_HOSTNAME 'cd workspace/GreenSense/Index/sketches/monitor/SoilMoistureSensorCalibratedSerial && sh pull-build-and-upload.sh'
+  sshpass -p $GARDEN_PASSWORD ssh -o StrictHostKeyChecking=no $GARDEN_USER@$GARDEN_HOSTNAME 'cd workspace/GreenSense/Index/sketches/monitor/SoilMoistureSensorCalibratedSerial && sh pull-build-and-upload.sh'
 else
   echo "Can't deploy dev branch. Switch to master."
 fi
