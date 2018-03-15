3e37436fdf255d0a92c02d953235d91e707980e6
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-15 23:11:10 +0000

    Trying to fix deployment

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,14 @@
 
 . ./garden-credentials.sh
 
-
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 if [ "$BRANCH" = "master" ]
 then
   echo "Deploying to host: $GARDEN_HOSTNAME"
+  
+  # Messy hack to get deployment working. This should be installed by default into the jenkins container but that doesnt seem to be working
+  sudo apt update && sudo apt -y install sshpass
 
   sshpass -p $GARDEN_PASSWORD ssh $GARDEN_USER:$GARDEN_PASSWORD@$GARDEN_HOSTNAME 'cd workspace/GreenSense/Index/sketches/monitor/SoilMoistureSensorCalibratedSerial && sh pull-build-and-upload.sh'
 else
