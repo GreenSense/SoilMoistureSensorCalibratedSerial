e8b2ec595050433b10bbcdb13b1564c5df8ebda7
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-15 22:27:37 +0000

    Restricted deploy script to dev branch

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,15 @@
 
 . ./garden-credentials.sh
 
-sshpass -p $GARDEN_PASSWORD ssh $GARDEN_USER:$GARDEN_PASSWORD@$GARDEN_HOSTNAME 'cd workspace/GreenSense/Index/sketches/monitor/SoilMoistureSensorCalibratedSerial && sh pull-build-and-upload.sh'
 
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+if [ "$BRANCH" = "master" ]
+then
+  echo "Deploying to host: $GARDEN_HOSTNAME"
+
+  sshpass -p $GARDEN_PASSWORD ssh $GARDEN_USER:$GARDEN_PASSWORD@$GARDEN_HOSTNAME 'cd workspace/GreenSense/Index/sketches/monitor/SoilMoistureSensorCalibratedSerial && sh pull-build-and-upload.sh'
+else
+  echo "Can't deploy dev branch. Switch to master."
+fi
 
