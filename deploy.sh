41a4687ade519c6c8eb53ffa6bb4eb0f608e6e4d
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-16 00:27:35 +0000

    Trying to get deploy script to work

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ then
   # Messy hack to get deployment working. This should be installed by default into the jenkins container but that doesnt seem to be working
 #  sudo apt-get update && sudo apt-get -y install sshpass
 
-  /usr/bin/sshpass -p "$GARDEN_PASSWORD" ssh -o StrictHostKeyChecking=no $GARDEN_USER@$GARDEN_HOSTNAME /bin/pwd
+  /usr/bin/sshpass -p "$GARDEN_PASSWORD" ssh -o StrictHostKeyChecking=no $GARDEN_USER@$GARDEN_HOSTNAME "cd /home/$GARDEN_USER/workspace/GreenSense/Index/sketches/monitor/SoilMoistureSensorCalibratedSerial && /bin/pwd"
 #  sshpass -p $GARDEN_PASSWORD ssh -o StrictHostKeyChecking=no -t $GARDEN_USER@$GARDEN_HOSTNAME "bash -c 'cd /home/$GARDEN_USER/workspace/GreenSense/Index/sketches/monitor/SoilMoistureSensorCalibratedSerial && sh pull-build-and-upload.sh'"
 
 else
