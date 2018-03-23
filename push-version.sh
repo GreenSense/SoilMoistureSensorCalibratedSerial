730ba481acf22eac0ae9859c34bfe962000ee6e1
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-23 01:31:59 +0000

    Updated push version script

--- push-version.sh
+++ push-version.sh
@@ -3,10 +3,16 @@
 . ./check-ci-skip.sh
 
 if [ $SKIP_CI = 0 ]; then
-    BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+  BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+  if [ "$BRANCH" = "dev" ];  then
 
     git commit buildnumber.txt -m "Updated version [ci skip]" && \
     git push origin $BRANCH --quiet
+  else
+    echo "Skipping push version. Only pushed for 'dev' branch not '$BRANCH'"
+  fi
 else
-    echo "Skipping push version [ci skip]"
+  echo "Skipping push version [ci skip]"
 fi
