03cf695fe0825d82db9ac512ecbc0bccc0c0ba57
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-23 01:04:12 +0000

    Made git quiet in push-version

--- push-version.sh
+++ push-version.sh
@@ -6,7 +6,7 @@ if [ $SKIP_CI = 0 ]; then
     BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
     git commit buildnumber.txt -m "Updated version [ci skip]" && \
-    git push origin $BRANCH
+    git push origin $BRANCH --quiet
 else
     echo "Skipping push version [ci skip]"
 fi
