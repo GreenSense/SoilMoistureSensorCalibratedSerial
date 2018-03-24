365e2314ec14326c2c89e4feef2c0cab99fea987
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-24 05:54:39 +0000

    Added port to pull build and upload script

--- pull-build-and-upload.sh
+++ pull-build-and-upload.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+echo ""
+echo "Pulling git changes, building, and uploading..."
+echo ""
+
+PORT_NAME=$1
+
+if [ ! $PORT_NAME ]; then
+  PORT_NAME="ttyUSB0"
+fi
+
+echo "Port: $PORT_NAME"
+
 git checkout master && \
 git pull origin master && \
 sh inject-version.sh && \
-sh build-and-upload.sh
+sh build-and-upload.sh $PORT_NAME
