debe9cb90505d711913688789a84be9f3b4a3e5d
CompulsiveCoder <compulsivecoder@gmail.com>
2018-04-09 07:34:07 +0000

    Updated prepare script

--- prepare.sh
+++ prepare.sh
@@ -30,8 +30,10 @@ fi
 
 # mono
 if ! type "msbuild" > /dev/null; then
+  VERSION_NAME=$(lsb_release -cs)
+
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-  sudo echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+  echo "deb http://download.mono-project.com/repo/ubuntu stable-$VERSION_NAME main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 
   sudo apt-get update -qq && sudo apt-get install -y mono-devel mono-complete ca-certificates-mono msbuild
 fi
