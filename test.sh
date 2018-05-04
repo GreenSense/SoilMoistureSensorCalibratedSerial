f351cdc1dcb2c3644cb5589e2f47af14ab889771
John <compulsivecoder@gmail.com>
2018-05-04 02:47:24 +0000

    Trying to fix skip ci

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
-
-. ./check-ci-skip.sh
-
 DIR=$PWD
 
-if [ $SKIP_CI = 1 ]; then
-  echo "Skipping test [ci skip]"
-else
-    cd tests/nunit/
-    sh build-and-test-all.sh && \
-    cd $DIR
-fi
+cd tests/nunit/
+sh build-and-test-all.sh && \
+cd $DIR
