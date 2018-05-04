73e9d00ecab1984c1d65f881e443d5b13067049b
John <compulsivecoder@gmail.com>
2018-05-04 10:14:00 +0000

    Reorganised sketch code and fixed tests

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,2 @@
-
-. ./check-ci-skip.sh
-
-if [ $SKIP_CI = 1 ]; then
-  echo "Skipping build [ci skip]"
-else
-  sh build.sh && \
-  sh build-tests.sh
-fi
+sh build.sh && \
+sh build-tests.sh
--- src/SoilMoistureSensorCalibratedSerial/Common.h
+++ src/SoilMoistureSensorCalibratedSerial/Common.h
@@ -1,6 +1,8 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
+extern const int ANALOG_MAX;
+
 extern long lastSerialOutputTime; // Milliseconds
 extern long serialOutputInterval; // Seconds
 
