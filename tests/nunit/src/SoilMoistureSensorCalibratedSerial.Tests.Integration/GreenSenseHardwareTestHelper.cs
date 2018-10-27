6d5a98cf94a2aeb688394026c1f2018471e546c8
John <compulsivecoder@gmail.com>
2018-10-27 04:28:52 +0000

    Tweaked margin of error in tests

--- tests/nunit/src/SoilMoistureSensorCalibratedSerial.Tests.Integration/GreenSenseHardwareTestHelper.cs
+++ tests/nunit/src/SoilMoistureSensorCalibratedSerial.Tests.Integration/GreenSenseHardwareTestHelper.cs
@@ -10,7 +10,7 @@ namespace SoilMoistureSensorCalibratedSerial.Tests.Integration
 
 		public int DelayAfterTurningSoilMoistureSensorOn = 3;
 
-		public int RawValueMarginOfError = 18;
+		public int RawValueMarginOfError = 25;
 		public int CalibratedValueMarginOfError = 3;
 		public double TimeErrorMargin = 0.2;
 
