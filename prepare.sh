9320692efdd024dedb4aaa319066c17892731ec2
John <compulsivecoder@gmail.com>
2018-11-02 05:52:33 +0000

    Fixed prepare script

--- prepare.sh
+++ prepare.sh
@@ -34,9 +34,10 @@ if ! type "pio" > /dev/null; then
 fi
 
 # mono
-if ! type "xbuild" > /dev/null; then
-  if [ "$USE_MONO4" = 0 ]; then
-    echo "Using latest mono"
+echo "USE_MONO4=$USE_MONO4"
+if [ "$USE_MONO4" = 0 ]; then
+  if ! type "msbuild" > /dev/null; then
+    echo "Installing latest mono"
     VERSION_NAME=$(lsb_release -cs)
   
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
@@ -44,8 +45,14 @@ if ! type "xbuild" > /dev/null; then
   
     sudo apt-get update -qq && sudo apt-get install -y mono-devel mono-complete ca-certificates-mono msbuild
   else
+    echo "Mono is already installed. Skipping install."
+  fi
+else
+  if ! type "xbuild" > /dev/null; then
     echo "Using mono4"
-    sudo apt-get update -qq && sudo apt-get install -y mono-devel mono-complete ca-certificates-mono
+    sudo apt-get install -y mono-devel mono-complete ca-certificates-mono
+  else
+    echo "Mono is already installed. Skipping install."
   fi
 fi
 
