c50e923d3904f48f35bd092b711698b9feabcfe0
John <compulsivecoder@gmail.com>
2018-11-02 05:47:35 +0000

    Trying to allow support for mono4 in CI

--- prepare.sh
+++ prepare.sh
@@ -35,12 +35,18 @@ fi
 
 # mono
 if ! type "xbuild" > /dev/null; then
-  VERSION_NAME=$(lsb_release -cs)
-
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-  echo "deb http://download.mono-project.com/repo/ubuntu stable-$VERSION_NAME main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-
-  sudo apt-get update -qq && sudo apt-get install -y mono-devel mono-complete ca-certificates-mono msbuild
+  if [ "$USE_MONO4" = 0 ]; then
+    echo "Using latest mono"
+    VERSION_NAME=$(lsb_release -cs)
+  
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    echo "deb http://download.mono-project.com/repo/ubuntu stable-$VERSION_NAME main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+  
+    sudo apt-get update -qq && sudo apt-get install -y mono-devel mono-complete ca-certificates-mono msbuild
+  else
+    echo "Using mono4"
+    sudo apt-get update -qq && sudo apt-get install -y mono-devel mono-complete ca-certificates-mono
+  fi
 fi
 
 cd tests/nunit && \
