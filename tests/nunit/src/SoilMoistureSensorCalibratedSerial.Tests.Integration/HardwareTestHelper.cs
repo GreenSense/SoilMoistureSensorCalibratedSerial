72a1e25d21e1bd430846e87d510596b9dc709499
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-22 00:51:45 +0000

    Trying to fix test

--- tests/nunit/src/SoilMoistureSensorCalibratedSerial.Tests.Integration/HardwareTestHelper.cs
+++ tests/nunit/src/SoilMoistureSensorCalibratedSerial.Tests.Integration/HardwareTestHelper.cs
@@ -22,7 +22,7 @@ namespace SoilMoistureSensorCalibratedSerial.Tests.Integration
         public string SimulatorPort;
         public int SimulatorBaudRate = 0;
 
-        public int DelayAfterConnectingToHardware = 1 * 1000;
+        public int DelayAfterConnectingToHardware = 2 * 1000;
         public int DelayAfterDisconnectingFromHardware = 500;
 
         public string DataPrefix = "D;";
